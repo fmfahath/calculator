743bdbba69d24661a28fcc48aea59904e2e37ff2
fmfahath <fmfahath.dev@gmail.com>
2023-01-11 13:07:52 +0530

    js : get values from active buttons and screen the value

---
+++ app.js
@@ -0,0 +1,19 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+   
+    //get values from active buttons and screen the value 
+   buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value += value;
+    })
+   });
+
+    
+
+
+
+})();
